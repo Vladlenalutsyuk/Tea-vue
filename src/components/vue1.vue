999e00e0eb0936a8e20e0d711afbb2904e09af79
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-10 12:45:57 +0300

    13

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,31 +1,24 @@
-
-
 <template>
     <div>
-		<input v-model.number="num" type="number">
-        <button @click="show">click</button>
+        <button @click="showCurrentDay">Показать текущий день</button>
     </div>
 </template>
+
 <script>
 export default {
-    data() {
-        return {
-			num: null,
-        };
-    },
-	methods: {
-                show: function() {
-                    let weekday = this.cape(this.getWeekday(this.num));
-					
-                    alert(weekday);
-                },
-				cape: function(str) {
-                    return str[0].toUpperCase() + str.slice(1);
-                },
-                getWeekday: function() {
-                    let days = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
-                    return days[this.num - 1] || "Неверное значение";
-                }
-            }
-        };
+    methods: {
+        showCurrentDay: function() {
+            let weekday = this.cape(this.getCurrentWeekday());
+            alert(weekday);
+        },
+        cape: function(str) {
+            return str[0].toUpperCase() + str.slice(1);
+        },
+        getCurrentWeekday: function() {
+            let days = ["воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота"];
+            let today = new Date().getDay();
+            return days[today] || "Неверное значение";
+        }
+    }
+};
 </script>
