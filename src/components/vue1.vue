c53dbbd63ccf7b548dfe692a65ce6dcb10419e5e
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-20 00:56:08 +0300

    32

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,28 +1,5 @@
 <template>
-  <table border="1">
-    <tr>
-      <th>Название</th>
-      <th>Цена</th>
-      <th>Количество</th>
-    </tr>
-    <tr v-for="(product, index) in products" :key="index">
-      <td>{{ product.name }}</td>
-      <td>{{ product.price }}</td>
-      <td>{{ product.quantity }}</td>
-    </tr>
-  </table>
+  <div>
+    <p v-for="num in 30" :key="num">{{ num }}</p>
+  </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      products: [
-        { name: 'product1', price: 100, quantity: 5 },
-        { name: 'product2', price: 200, quantity: 4 },
-        { name: 'product3', price: 300, quantity: 3 }
-      ]
-    };
-  }
-};
-</script>
