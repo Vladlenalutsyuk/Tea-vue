aafca8138e4f6a488cb5e9ddbcf3e7faa082fc6f
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-20 00:57:54 +0300

    33

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <p v-for="num in 30" :key="num">{{ num }}</p>
-  </div>
+  <ul>
+    <template v-for="item in items">
+      <li v-if="item > 0" :key="item">{{ item }}</li>
+    </template>
+  </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [1, -2, 3, -4, 5]
+    };
+  }
+};
+</script>
