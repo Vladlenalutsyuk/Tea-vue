dec731770dedfcdec114a0ad154e48c24bb3fc9d
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-20 00:09:01 +0300

    17

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,12 +1,19 @@
 <template>
-  <button @click.once="handleClick">Кликни меня</button>
+  <p v-if="visible">Первый абзац, когда visible = true</p>
+  <p v-else>Второй абзац, когда visible = false</p>
+  <button @click="toggleVisible">Переключить видимость</button>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      visible: true, // Стартовое значение
+    };
+  },
   methods: {
-    handleClick() {
-      alert("Первый клик!");
+    toggleVisible() {
+      this.visible = !this.visible; // Переключаем значение visible
     },
   },
 };
