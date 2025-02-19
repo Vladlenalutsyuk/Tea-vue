49a7a070c33083f12c2f6294fa635be42936a5d1
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-20 00:19:52 +0300

    20

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,24 +1,32 @@
 <template>
-  <p v-if="isVisible">Это абзац</p>
-  
-  <!-- Показываем кнопку в зависимости от состояния isVisible -->
-  <button v-if="!isVisible" @click="showParagraph">Показать абзац</button>
-  <button v-if="isVisible" @click="hideParagraph">Скрыть абзац</button>
+  <button @click="toggle(1)">Toggle Абзац 1</button>
+  <p v-if="visible1">Это первый абзац</p>
+
+  <button @click="toggle(2)">Toggle Абзац 2</button>
+  <p v-if="visible2">Это второй абзац</p>
+
+  <button @click="toggle(3)">Toggle Абзац 3</button>
+  <p v-if="visible3">Это третий абзац</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      isVisible: false, // Абзац изначально скрыт
+      visible1: false, // Статус видимости первого абзаца
+      visible2: false, // Статус видимости второго абзаца
+      visible3: false, // Статус видимости третьего абзаца
     };
   },
   methods: {
-    showParagraph() {
-      this.isVisible = true; // Показываем абзац
-    },
-    hideParagraph() {
-      this.isVisible = false; // Скрываем абзац
+    toggle(number) {
+      if (number === 1) {
+        this.visible1 = !this.visible1;
+      } else if (number === 2) {
+        this.visible2 = !this.visible2;
+      } else if (number === 3) {
+        this.visible3 = !this.visible3;
+      }
     },
   },
 };
