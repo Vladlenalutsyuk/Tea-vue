06150664eb9a531d37040b37246ad6298f6402ca
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-20 00:22:21 +0300

    22

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,42 +1,17 @@
 <template>
-  <!-- Кнопка для первого абзаца -->
-  <button @click="toggle(1)">
-    {{ visible1 ? 'Скрыть абзац 1' : 'Показать абзац 1' }}
-  </button>
-  <p v-if="visible1">Это первый абзац</p>
-
-  <!-- Кнопка для второго абзаца -->
-  <button @click="toggle(2)">
-    {{ visible2 ? 'Скрыть абзац 2' : 'Показать абзац 2' }}
-  </button>
-  <p v-if="visible2">Это второй абзац</p>
-
-  <!-- Кнопка для третьего абзаца -->
-  <button @click="toggle(3)">
-    {{ visible3 ? 'Скрыть абзац 3' : 'Показать абзац 3' }}
-  </button>
-  <p v-if="visible3">Это третий абзац</p>
+  <!-- Если isAdmin равно true, показываем первый текст -->
+  <p v-if="isAdmin">Пользователь является администратором</p>
+  
+  <!-- Если isAdmin равно false, показываем второй текст -->
+  <p v-else>Пользователь не является администратором</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      visible1: false, // Статус видимости первого абзаца
-      visible2: false, // Статус видимости второго абзаца
-      visible3: false, // Статус видимости третьего абзаца
+      isAdmin: true, // здесь может быть true или false
     };
-  },
-  methods: {
-    toggle(number) {
-      if (number === 1) {
-        this.visible1 = !this.visible1;
-      } else if (number === 2) {
-        this.visible2 = !this.visible2;
-      } else if (number === 3) {
-        this.visible3 = !this.visible3;
-      }
-    },
   },
 };
 </script>
