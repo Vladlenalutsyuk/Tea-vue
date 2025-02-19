4a51dc21dcea0aab06bc7cee5d57dabc2094300a
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-19 23:53:27 +0300

    14_2

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,13 +1,19 @@
 <template>
   <p>{{ text }}</p>
+  <button @click="changeText">Изменить текст</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      text: "Пример текста",
+      text: "Исходный текст",
     };
+  },
+  methods: {
+    changeText() {
+      this.text = "Новый текст";
+    },
   },
 };
 </script>
