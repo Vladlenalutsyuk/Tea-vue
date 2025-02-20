6732d993a6e5888ed53f3e66377ffaa6070a5493
Vladlenalutsyuk <lucukvladlena@gmail.com>
2025-02-21 01:30:59 +0300

    42_1

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,37 +1,4 @@
 <template>
-  <div>
-    <p :class="classes">Этот текст зависит от свойств</p>
-  </div>
+  <p :style="{ color: 'green', backgroundColor: 'yellow' }">
+  </p>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      isValid: true,  // Если true, применяется класс valid
-      isDisabled: true,  // Если true, применяется класс disabled
-    };
-  },
-  computed: {
-    // Создаем вычисляемое свойство для классов
-    classes() {
-      return {
-        valid: this.isValid,    // Применяется класс valid, если isValid === true
-        disabled: this.isDisabled,  // Применяется класс disabled, если isDisabled === true
-      };
-    },
-  },
-};
-</script>
-
-<style>
-.valid {
-  color: green;
-  font-weight: bold;
-}
-
-.disabled {
-  color: gray;
-  text-decoration: line-through;
-}
-</style>
