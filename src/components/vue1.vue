f1cada135ba1ecff0d2eb955dd101ddb4a95b22c
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-21 02:41:18 +0300

    51_1

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,38 +1,25 @@
 <template>
   <div>
-    <p>Выберите день, месяц и год:</p>
-    
-    <select v-model="selectedDay">
-      <option v-for="i in 31" :key="i">{{ i }}</option>
-    </select>
-    
-    <select v-model="selectedMonth">
-      <option v-for="month in months" :key="month">{{ month }}</option>
-    </select>
-    
-    <select v-model="selectedYear">
-      <option v-for="year in years" :key="year">{{ year }}</option>
-    </select>
-    
-    <p>Вы выбрали: {{ selectedDay }} {{ selectedMonth }} {{ selectedYear }}</p>
+    <!-- Инпут, который будет блокироваться и разблокироваться -->
+    <input v-bind:disabled="isDisabled" type="text" placeholder="Введите текст">
+
+    <!-- Кнопка для переключения блокировки инпута -->
+    <button @click="toggleInput">Переключить блокировку</button>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    const currentDate = new Date();
-    const currentDay = currentDate.getDate();
-    const currentMonth = currentDate.getMonth(); // Январь это 0
-    const currentYear = currentDate.getFullYear();
-
     return {
-      selectedDay: currentDay,  // Устанавливаем текущий день
-      selectedMonth: this.months[currentMonth],  // Устанавливаем текущий месяц
-      selectedYear: currentYear,  // Устанавливаем текущий год
-      months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-      years: [2020, 2021, 2022, 2023, 2024],
+      isDisabled: false,  // Изначально инпут не заблокирован
     };
   },
+  methods: {
+    // Метод для переключения состояния блокировки инпута
+    toggleInput() {
+      this.isDisabled = !this.isDisabled;  // Меняем состояние на противоположное
+    }
+  }
 };
 </script>
