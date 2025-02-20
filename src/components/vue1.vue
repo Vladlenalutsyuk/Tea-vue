e08c475f188a65aebd775ae69c019d57b2cdf9ef
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-21 02:33:48 +0300

    47_1

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,16 +1,1 @@
-<template>
-  <div>
-    <input type="checkbox" v-model="checked"> Показать абзац
-    <p v-if="checked">Этот абзац виден, когда чекбокс отмечен ✅</p>
-  </div>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      checked: false
-    };
-  }
-};
-</script>
+Выведите полученный в предыдущей задаче массив языков в виде списка ul.
