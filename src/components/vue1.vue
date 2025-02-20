f5e55106ed7ab8f584d1c3372e83500c5b70f872
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-21 02:34:28 +0300

    47_2

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,1 +1,25 @@
-Выведите полученный в предыдущей задаче массив языков в виде списка ul.
+<template>
+  <div>
+    <p>Какие языки вы знаете?</p>
+    <input type="checkbox" v-model="languages" value="English"> Английский
+    <input type="checkbox" v-model="languages" value="Spanish"> Испанский
+    <input type="checkbox" v-model="languages" value="French"> Французский
+    <input type="checkbox" v-model="languages" value="German"> Немецкий
+    <input type="checkbox" v-model="languages" value="Russian"> Русский
+
+    <p>Вы знаете следующие языки:</p>
+    <ul>
+      <li v-for="(language, index) in languages" :key="index">{{ language }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      languages: []
+    };
+  }
+};
+</script>
