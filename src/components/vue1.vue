c3d8c1b19e02bfeb61255e2b52e623ee8f15780a
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-21 02:46:14 +0300

    53_1

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <!-- Ссылка, по клику на которую будет выводиться текст -->
-    <a href="#" @mousedown="handleMouseClick">Нажмите на меня</a>
+    <!-- Инпут для ввода нового элемента -->
+    <input v-model="newItem" placeholder="Введите новый элемент">
 
-    <!-- Абзац, в который будет выведен текст -->
-    <p>{{ message }}</p>
+    <!-- Кнопка для добавления элемента -->
+    <button @click="addItem">Добавить</button>
+
+    <!-- Список элементов массива -->
+    <ul>
+      <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,18 +19,19 @@
 export default {
   data() {
     return {
-      message: '',  // Сообщение для отображения
+      // Свойство для хранения введенного текста
+      newItem: '',
+      
+      // Начальный массив
+      items: ['item1', 'item2', 'item3'],
     };
   },
   methods: {
-    // Метод для обработки нажатия кнопки мыши
-    handleMouseClick(event) {
-      if (event.button === 0) {
-        this.message = 'left';  // Левая кнопка
-      } else if (event.button === 1) {
-        this.message = 'middle';  // Средняя кнопка
-      } else if (event.button === 2) {
-        this.message = 'right';  // Правая кнопка
+    // Метод для добавления нового элемента в конец массива
+    addItem() {
+      if (this.newItem.trim() !== '') {
+        this.items.push(this.newItem);  // Добавляем элемент в конец массива
+        this.newItem = '';  // Очищаем инпут
       }
     }
   }
