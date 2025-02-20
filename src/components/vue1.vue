0f562af2a83caf223d91936c12d8425c29960a92
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-21 02:29:04 +0300

    44_2

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <input v-model.number="num" type="number" placeholder="Введите число">
-    <button @click="calculateSqrt">Вычислить корень</button>
-    <p>Квадратный корень: {{ result }}</p>
+    <input v-model.number="num1" type="number" placeholder="Введите первое число">
+    <input v-model.number="num2" type="number" placeholder="Введите второе число">
+    <button @click="calculateSum">Посчитать сумму</button>
+    <p>Сумма: {{ result }}</p>
   </div>
 </template>
 
@@ -10,13 +11,14 @@
 export default {
   data() {
     return {
-      num: 0,
+      num1: 0,
+      num2: 0,
       result: 0
     };
   },
   methods: {
-    calculateSqrt() {
-      this.result = Math.sqrt(this.num);
+    calculateSum() {
+      this.result = this.num1 + this.num2;
     }
   }
 };
