70073f9c52ec608fb63d8dc3bdc6f9ddb626ee31
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-21 20:46:05 +0300

    55

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -1,9 +1,26 @@
 <template>
-	<ul>
-		<li v-for="(item, index) in items" :key="item" @click="removeItem(index)">
-			{{ item }}
-		</li>
-	</ul>
+	<table>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Имя</th>
+				<th>Зарплата</th>
+				<th>Возраст</th>
+				<th>Действия</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr v-for="user in users" :key="user.id">
+				<td>{{ user.id }}</td>
+				<td>{{ user.name }}</td>
+				<td>{{ user.salary }}</td>
+				<td>{{ user.age }}</td>
+				<td>
+					<a href="#" @click.prevent="removeUser(user.id)">Удалить</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </template>
 
 <script>
@@ -11,24 +28,45 @@ import { ref } from "vue";
 
 export default {
 	setup() {
-		const items = ref(["a", "b", "c", "d", "e"]);
+		const users = ref([
+			{ id: 1, name: "name1", salary: 100, age: 30 },
+			{ id: 2, name: "name2", salary: 200, age: 40 },
+			{ id: 3, name: "name3", salary: 300, age: 50 },
+		]);
 
-		const removeItem = (index) => {
-			items.value = items.value.filter((_, i) => i !== index);
+		const removeUser = (id) => {
+			users.value = users.value.filter((user) => user.id !== id);
 		};
 
-		return { items, removeItem };
+		return { users, removeUser };
 	},
 };
 </script>
 
 <style scoped>
-li {
-	cursor: pointer;
-	user-select: none;
-	transition: opacity 0.2s ease-in-out;
+table {
+	width: 100%;
+	border-collapse: collapse;
+	margin-top: 20px;
 }
-li:hover {
-	opacity: 0.6;
+
+th, td {
+	border: 1px solid #ddd;
+	padding: 8px;
+	text-align: left;
+}
+
+th {
+	background-color: #f4f4f4;
+}
+
+a {
+	color: red;
+	cursor: pointer;
+	text-decoration: none;
+}
+
+a:hover {
+	text-decoration: underline;
 }
 </style>
