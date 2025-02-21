7e6b7fb6cae461978fe16e05a9de88b2b29eda63
Vladlenalutsyuk <lutsyuk.v.i.23@gmail.com>
2025-02-21 20:47:30 +0300

    56

--- src/components/vue1.vue
+++ src/components/vue1.vue
@@ -4,20 +4,33 @@
 			<tr>
 				<th>ID</th>
 				<th>Имя</th>
-				<th>Зарплата</th>
-				<th>Возраст</th>
+				<th>Фамилия</th>
 				<th>Действия</th>
 			</tr>
 		</thead>
 		<tbody>
 			<tr v-for="user in users" :key="user.id">
-				<td>{{ user.id }}</td>
-				<td>{{ user.name }}</td>
-				<td>{{ user.salary }}</td>
-				<td>{{ user.age }}</td>
-				<td>
-					<a href="#" @click.prevent="removeUser(user.id)">Удалить</a>
-				</td>
+				<!-- Обычный режим (не редактирование) -->
+				<template v-if="editingUser?.id !== user.id">
+					<td>{{ user.id }}</td>
+					<td>{{ user.name }}</td>
+					<td>{{ user.surn }}</td>
+					<td>
+						<a href="#" @click.prevent="edit(user)">Редактировать</a> |
+						<a href="#" @click.prevent="remove(user.id)">Удалить</a>
+					</td>
+				</template>
+
+				<!-- Режим редактирования -->
+				<template v-else>
+					<td>{{ user.id }}</td>
+					<td><input v-model="editingUser.name" placeholder="Имя"></td>
+					<td><input v-model="editingUser.surn" placeholder="Фамилия"></td>
+					<td>
+						<a href="#" @click.prevent="save(user)">Сохранить</a> |
+						<a href="#" @click.prevent="cancel()">Отмена</a>
+					</td>
+				</template>
 			</tr>
 		</tbody>
 	</table>
@@ -29,16 +42,35 @@ import { ref } from "vue";
 export default {
 	setup() {
 		const users = ref([
-			{ id: 1, name: "name1", salary: 100, age: 30 },
-			{ id: 2, name: "name2", salary: 200, age: 40 },
-			{ id: 3, name: "name3", salary: 300, age: 50 },
+			{ id: 1, name: "name1", surn: "surn1" },
+			{ id: 2, name: "name2", surn: "surn2" },
+			{ id: 3, name: "name3", surn: "surn3" },
 		]);
 
-		const removeUser = (id) => {
-			users.value = users.value.filter((user) => user.id !== id);
+		const editingUser = ref(null);
+		const originalUser = ref(null);
+
+		const edit = (user) => {
+			editingUser.value = { ...user }; // Создаем копию объекта
+			originalUser.value = { ...user }; // Сохраняем оригинальные данные
 		};
 
-		return { users, removeUser };
+		const save = (user) => {
+			Object.assign(user, editingUser.value); // Обновляем объект
+			editingUser.value = null; // Выходим из режима редактирования
+			originalUser.value = null;
+		};
+
+		const cancel = () => {
+			editingUser.value = null;
+			originalUser.value = null;
+		};
+
+		const remove = (id) => {
+			users.value = users.value.filter(user => user.id !== id);
+		};
+
+		return { users, editingUser, edit, save, cancel, remove };
 	},
 };
 </script>
@@ -61,12 +93,17 @@ th {
 }
 
 a {
-	color: red;
+	color: blue;
 	cursor: pointer;
 	text-decoration: none;
+	margin-right: 10px;
 }
 
 a:hover {
 	text-decoration: underline;
+}
+
+input {
+	padding: 5px;
 }
 </style>
